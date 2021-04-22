af4a90d3db40f1622c56ac0c073a9919db26625b
MAC <arinze086@gmail.com>
2021-04-22 23:34:55 +0100

    Array prototype return issues

--- src/components/job-board/Board.js
+++ src/components/job-board/Board.js
@@ -1,22 +1,9 @@
-import React, { Children, useState } from 'react'
-import { Link } from "react-router-dom";
+import React  from 'react'
 import FeatherIcon from 'feather-icons-react';
 import "./board.css";
 import { Button } from '../buttons/Button'
 
-// id:0,
-//         company: "ECi Software Solutions",
-//         datePosted: "2020-11-11",
-//         country: "Canada",
-//         city: 'Ontario',
-//         iLike: true,
-//         aboutCompany: 'For more than 30 years, ECI Software Solutions has been providing industry-specific, cloud-based business management software and services to small and medium-sized businesses. With divisions focused on manufacturing, wholesale/retail distribution, building and construction, and field service, ECIs solutions integrate into every aspect of a customers business to help them level the playing field, run day-to-day operations more efficiently, and free them up to focus on what matters most. It’s how business gets done.',
-//         jobDescription: 'To apply for this position, please attach a detailed resume that demonstrates your qualifications and skill set pertaining to this position. Applications without a resume will not be considered. ECI Lasso is seeking a Frontend Developer to continue to provide the best in Software Solutions to small and medium-sized businesses ECI Lasso is the developer of the leading cloud-based, on-demand Customer Relationship Management (CRM) software for the new-home-building industry. We have experienced consistent growth during the last decade and our user base asks a lot of our system. Scale, reliability, and innovation keep us learning on a daily basis. You will join our front-end team to enhance and maintain our user interfaces. We have distributed systems hosted in AWS, which are presented to our users through an Angular/React frontend.',
-//         jobTitle: 'Frontend developer',
-//         responsibility: 'You thrive in a self-directed and open work environment. There’s no boss breathing down your neck, no deadlines, no fixed work hours and no in-office requirements. It’s really important you can manage your time well, and know what you need in order to do your job well (e.g. what work hours to pick so you have enough time to communicate with team members effectively) The ability to balance of maintainable code with delivering value regularly to our customers You can communicate the trade-offs of doing something quickly and doing something well Experience building complex interfaces with state management',
-//         marketType: 'Technology Occupations',
-//         employmentType: 'Full time',
-//         skills
+
 
 export const Board = ({ id, JobTitle, company, datePosted, country, city, iLike, aboutCompany, jobDescription,
     responsibility, marketType, employmentType, skills, visible, props }) => {
